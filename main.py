49d89657aeba6161e90af96c56fa27e6d690d2f3
lrnholanda <lrnholanda@gmail.com>
2021-03-11 21:23:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+requested_topping = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_topping:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_topping:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_topping:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+#Alien game
+
+alien_color = 'blue'
+
+if alien_color == 'green':
+    print("you just earned 5 points")
+elif alien_color == 'yellow':
+    print(" you just earned 10 points")
+elif alien_color == 'red':
+    print(" you just earned 15 points")
+elif alien_color == 'blue':
+    print("\nyou just earned 20 points")    
+
+#Person's stage of life
+
+person = 24
+
+if person < 2:
+  print("the person is a baby.")
+elif person < 4:
+  print("the person is a toddler.")
+elif person < 13:
+  print("the person is a kid.")
+elif person < 20:
+  print("the person is a teenager.")
+elif person < 65:
+  print("\nthe person is an adult.")
+elif person >= 65:
+  print("the person is an elser.")        
+
+#favorit fruit
+
+frutas = ['banana', 'abacaxi', 'pêra', 'goiaba']
+
+if 'banana' in frutas:
+  print("\nYou really like bananas!")
+if 'abacaxi' in frutas:
+  print("You really like abacaxi!")
+if 'goiaba' in frutas:
+  print("you really like goiaba!")    
